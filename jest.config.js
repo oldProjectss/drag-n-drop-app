b973a70b3effee98ab0101685653cf3a03388ca0
Ismail Courr <contact@ismailcourr.ml>
2023-04-13 23:39:24 +0000

    Fix Jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.{ts,tsx,js,jsx}', '<rootDir>/src/**/*.{spec,test}.{ts,tsx,js,jsx}'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
